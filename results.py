73e76cfe536b65e81418421dde3be5e7b521dfd2
Austin Flynt <aflynt1@gmail.com>
2024-11-26 16:34:01 -0600

    final grades

--- results.py
+++ results.py
@@ -8,6 +8,11 @@ hw = {
     5: [ 70, 70],
     6: [ 82, 82],
     7: [ 44, 45],
+    8: [ 72, 72],
+    9: [ 60, 60],
+    10:[ 77, 78],
+    11:[ 78, 78],
+    12:[ 80, 80],
 }
 
 count = len(hw)
@@ -21,7 +26,21 @@ for n,res in hw.items():
     gsum.append(g)
     print(f"HW #{n:2d}: {pi:3d}/{ptot:3d} = {g*100:10.2f}%")
 
-grade = sum(gsum)/count
-print(f"HW grade: {grade*100:10.1f} %")
+hw_grade = sum(gsum)/count
+print(f"HW grade: {hw_grade*100:10.1f} %")
 
 
+
+grades = {
+    "HW": (hw_grade, 0.33),
+    "MID": (0.915, 0.33),
+    "FINAL": (0.80, 0.34),
+}
+
+class_grade = 0
+
+for k,v in grades.items():
+    g,per = v
+    class_grade += g*per
+
+print(f"class grade: {class_grade}")
