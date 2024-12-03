ac03861caadbcc70a091d0b77a8906343b8291b4
Austin Flynt <aflynt1@gmail.com>
2024-12-03 17:40:30 -0600

    added oblique shocks

--- gui_normal_shocks.py
+++ gui_normal_shocks.py
@@ -112,6 +112,32 @@ def norm_shock_calc(x: float, intype:str , rdict: dict):
     except Exception as e:
         print(f"error: M1: {M1} e: {str(e)}")
         traceback.print_exc()
+      
+def oblique_shock_calc(x: float, intype: str, rdict: dict):
+  try:
+    M1 = float(mach_spinner.get())
+    if intype == 'turn angle weak':
+      theta = x*m.pi/180  # rad
+      M2 = gd.oblique_m2(M1, theta)
+      wave_angle = gd.oblique_beta_zero(M1, theta) *180/m.pi
+      PR = gd.oblique_ratio_p(M1, theta)
+      RR = gd.oblique_ratio_rho(M1, theta)
+      TR = gd.oblique_ratio_t(M1, theta)
+      PTR = gd.oblique_ratio_pt(M1, theta)
+    else:
+      M2 = x
+
+    rdict['M2'].resstr.set(   f"{float(M2):15.6f}") 
+    rdict['Turn Angle'].resstr.set(f"{x:15.6f}")
+    rdict['Wave Angle'].resstr.set(f"{wave_angle:15.6f}")
+    rdict['P2/P1'].resstr.set(f"{PR:15.6f}")
+    rdict['rho2/rho1'].resstr.set(f"{RR:15.6f}")
+    rdict['T2/T1'].resstr.set(f"{TR:15.6f}")
+    rdict['Pt2/Pt1'].resstr.set(f"{PTR:15.6f}")
+
+  except Exception as e:
+    pass
+
 
 class results_pair:
 
@@ -126,7 +152,7 @@ class results_pair:
         self.rlabel.grid(row=self.row, column=2, sticky=E)
 
 class GenPack:
-   def __init__(self, frame:ttk.Frame, labeltext:str, result_list:List[str], combo_list:List[str], result_callback: Callable[[float, str, Dict], None]):
+   def __init__(self, frame:ttk.Frame, labeltext:str, result_list:List[str], combo_list:List[str], result_callback: Callable[[float, str, Dict], None], ioffset=3):
     self.frame = frame
 
     self.frame.grid(column=0, row=0, sticky=(N, S, E, W))
@@ -137,14 +163,14 @@ class GenPack:
     self.frame.rowconfigure(2, weight=1)
 
     for i in range(len(result_list)):
-      self.frame.rowconfigure(i+3, weight=5)
+      self.frame.rowconfigure(i+ioffset, weight=5)
 
     self.itl = ttk.Label(self.frame, text=labeltext, style="BW.TLabel", font=("Kozuka Mincho Pro M", 12, "bold"))
     self.itl.grid(row=1, column=1, sticky=(E,W))
 
     self.irdict = {}
     for i,resname in enumerate(result_list):
-       self.irdict[resname] = results_pair(self.frame, i+3, resname)
+       self.irdict[resname] = results_pair(self.frame, i+ioffset, resname)
 
     # Combobox
     #self.incb = ttk.Combobox(self.frame, textvariable=self.irdict['type'].resstr)
@@ -155,7 +181,7 @@ class GenPack:
     self.incb.state(['readonly'])
 
     # Input Entry
-    self.i_entry_x = ttk.Spinbox(self.frame, width=7, from_=0.0, to=10, increment=0.1)
+    self.i_entry_x = ttk.Spinbox(self.frame, width=7, from_=0.0, to=45, increment=0.1)
     self.i_entry_x.set("1.1")
     self.i_entry_x.grid(row=2, column=2, sticky=(W,E))
     
@@ -167,7 +193,7 @@ class GenPack:
     self.i_entry_x.bind("<<Decrement>>", self.ret_wrapper)
     
     # Go Button
-    ttk.Button(self.frame, text="Calculate", command=self.ifcmd).grid(row=2, column=3, sticky=(W,E))
+    ttk.Button(self.frame, text="Calculate", command=self.ifcmd).grid(row=2, column=ioffset, sticky=(W,E))
     
     for child in self.frame.winfo_children(): 
       child.grid_configure(padx=5, pady=5)
@@ -179,10 +205,10 @@ root.title("Gas Dynamics")
 root.configure(background='black')
 
 style = ttk.Style(root)
-style.configure('TFrame', background='#00005F')
+style.configure('TFrame', background='#080808')
 #style.configure('TLabel', background='#00005F', foreground="#AFAF00", font=("Fixedsys", 12))
-style.configure('TLabel', background='#00005F', foreground="#AFAF00", font=("Kozuka Mincho Pro M", 11))
-style.configure('TPanedWindow', background='#00005F')
+style.configure('TLabel', background='#080808', foreground="#AFAF00", font=("Kozuka Mincho Pro M", 11))
+style.configure('TPanedWindow', background='#080808')
 #style.configure('TButton', background='#808080', foreground="#AFAF00")
 #style.configure('TCombobox', foreground="blue", background="yellow", fieldbackground="yellow", selectbackground="yellow")
 #                #foreground="black",
@@ -191,6 +217,7 @@ style.configure('TPanedWindow', background='#00005F')
 #                selectbackground='#lightgray',
 #)
 #style.configure('TSpinbox', selectbackground='#800080',fieldbackground='#800080', foreground="#AFAF00")
+
 
 # make paned windows
 mp = ttk.PanedWindow(root, orient=VERTICAL)
@@ -201,24 +228,64 @@ P1.columnconfigure(0, weight=1)
 P1.rowconfigure(0, weight=1)
 mp.add(P1)
 
-P2 = ttk.PanedWindow(mp, orient=VERTICAL)
-P2.columnconfigure(0, weight=1)
-P2.rowconfigure(0, weight=1)
-mp.add(P2)
+#P2 = ttk.PanedWindow(mp, orient=VERTICAL)
+#P2.columnconfigure(0, weight=1)
+#P2.rowconfigure(0, weight=1)
+#mp.add(P2)
+
+P3 = ttk.PanedWindow(mp, orient=VERTICAL)
+P3.columnconfigure(0, weight=1)
+P3.rowconfigure(0, weight=1)
+mp.add(P3)
 
 
 # ISENTROPICS -------------------------------------
-fis = ttk.Frame(P1, padding="3 3 12 12")
-P1.add(fis)
+f1 = ttk.Frame(P1, padding="3 3 12 12")
+P1.add(f1)
 irlist = ['M','Pt/p','p/Pt','Tt/T','T/Tt','PM-angle','Mach angle','P/P*','T/T*','A/A*','type',]
 iclist = ('M', 'T/Tt', 'P/Pt', 'A/A*-','A/A*+', 'PM-angle (deg)')
-GenPack(fis, "Isentropic Flow", irlist, iclist, isen_calc)
+GenPack(f1, "Isentropic Flow", irlist, iclist, isen_calc)
 
 # NORMAL SHOCKS -------------------------------------
-fns = ttk.Frame(P2, padding="3 3 12 12")
-P2.add(fns)
-nrlist = ['M1','M2','Pt2/Pt1','P2/P1','T2/T1','r2/r1','p1/pt2',]
-nclist = ('M1', 'M2', 'P2/P1', 'rho2/rho1','T2/T1', 'Pt2/Pt1', 'p1/pt2')
-GenPack(fns, "Normal Shocks", nrlist, nclist, norm_shock_calc)
+#f2 = ttk.Frame(P2, padding="3 3 12 12")
+#P2.add(f2)
+#nrlist = ['M1','M2','Pt2/Pt1','P2/P1','T2/T1','r2/r1','p1/pt2',]
+#nclist = ('M1', 'M2', 'P2/P1', 'rho2/rho1','T2/T1', 'Pt2/Pt1', 'p1/pt2')
+#GenPack(f2, "Normal Shocks", nrlist, nclist, norm_shock_calc)
+
+# OBLIQUE SHOCKS -----------------------------------
+
+f3 = ttk.Frame(P3, padding="3 3 12 12")
+P3.add(f3)
+#rlist3 = ['M1','M2','Pt2/Pt1','P2/P1','T2/T1','r2/r1','p1/pt2',]
+rlist3 = [
+   'M2',
+   'Turn Angle',
+   'Wave Angle',
+   'P2/P1',
+   'rho2/rho1',
+   'T2/T1',
+   'Pt2/Pt1',
+#   'M1n',
+#   'M2n',
+]
+clist3 = (
+   'turn angle weak',
+   'turn angle strong',
+   'wave angle',
+   'M1n',
+)
+gpo = GenPack(f3, "Oblique Shocks", rlist3, clist3, oblique_shock_calc, 4)
+
+
+gpo.mach_label = ttk.Label(f3, text="M1:", style="BW.TLabel", font=("Kozuka Mincho Pro M", 11, "bold"))
+gpo.mach_label.grid(row=3, column=1, sticky=E, padx=5, pady=5)
+gpo.mach_label.configure(background="#00005F", foreground="#7E5F7E")
+gpo.i_entry_x.set("20")
+
+mach_spinner = ttk.Spinbox(f3, width=7, from_=1.0, to=10, increment=0.25)
+mach_spinner.set("5.0")
+mach_spinner.grid(row=3, column=2, sticky=(W,E), padx=5, pady=5)
+
 
 root.mainloop()
